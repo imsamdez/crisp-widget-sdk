96a49955209811883f86574e9a10cdd4bd902e8c
sdubrez <22425976+imsamdez@users.noreply.github.com>
2024-10-17 18:25:02 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  // Crisp Widget SDK
+  useEffect(() => {
+    (function () {
+      setTimeout(() => {
+        var s = document.createElement("script");
+
+        s.src = "https://assets.crisp.chat/widget/javascripts/sdk.min.js";
+        document.getElementsByTagName("head")[0].appendChild(s);
+      }, 1000);
+    })();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="s-crisp-widget">
+      <button
+        onClick={() => {
+          console.log(window.location.href);
+          window.$crisp.setHeight(300);
+          window.$crisp.showToast("success", "Hello from toast");
+          console.log(window.$crisp);
+        }}
+      >
+        Show toast
+      </button>
     </div>
   );
 }
